015aeacba5db9ecb8531df2423e3c74ca9a2fd88
D-lemillion <dyanafrnd@gmail.com>
2024-06-15 16:04:48 +0700

    update 8

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,3 +90,12 @@ window.addEventListener("load", function() {
     // Sembunyikan elemen loading setelah halaman dimuat
     loader.style.display = "none";
 });
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/666d48a49a809f19fb3e11f6/1i0de7gqo';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
